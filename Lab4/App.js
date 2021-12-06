102a4a04b2368fae4399045b349ca62392afecc1
Robert Kolke <robertoskarkolke@gmail.com>
2021-12-06 11:31:02 +0100

    Lab 4 gotowe (bez Readme.MD)

---
+++ Lab4/App.js
@@ -0,0 +1,233 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const scoreDisplay = document.getElementById('score')
+  const width = 28
+  let score = 0
+  const grid = document.querySelector('.grid')
+  const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+  ]
+  // 0 - monety
+  // 1 - ściany
+  // 2 - leżę duchów
+  // 3 - moneta mocy 
+  // 4 - puste
+
+  const squares = []
+
+  //tworzenie planszy
+  function createBoard() {
+    for (let i = 0; i < layout.length; i++) {
+      const square = document.createElement('div')
+      grid.appendChild(square)
+      squares.push(square)
+
+      //dodanie układu planszy
+      if(layout[i] === 0) {
+        squares[i].classList.add('pac-dot')
+      } else if (layout[i] === 1) {
+        squares[i].classList.add('wall')
+      } else if (layout[i] === 2) {
+        squares[i].classList.add('ghost-lair')
+      } else if (layout[i] === 3) {
+        squares[i].classList.add('power-pellet')
+      }
+    }
+  }
+  createBoard()
+
+
+  //tworzenie postaci
+  //rysowanie Pac-Mana
+  let pacmanCurrentIndex = 490
+  squares[pacmanCurrentIndex].classList.add('pac-man')
+  //zbieranie koordynatów pacmana na osi x oraz y
+  // function getCoordinates(index) {
+  //   return [index % width, Math.floor(index / width)]
+  // }
+
+  // console.log(getCoordinates(pacmanCurrentIndex))
+
+  //poruszanie Pac-Mana
+  function movePacman(e) {
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+    switch(e.keyCode) {
+      case 37:
+        if(
+          pacmanCurrentIndex % width !== 0 &&
+          !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')
+          )
+        pacmanCurrentIndex -= 1
+        if (squares[pacmanCurrentIndex -1] === squares[363]) {
+          pacmanCurrentIndex = 391
+        }
+        break
+      case 38:
+        if(
+          pacmanCurrentIndex - width >= 0 &&
+          !squares[pacmanCurrentIndex -width].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')
+          ) 
+        pacmanCurrentIndex -= width
+        break
+      case 39:
+        if(
+          pacmanCurrentIndex % width < width - 1 &&
+          !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')
+        )
+        pacmanCurrentIndex += 1
+        if (squares[pacmanCurrentIndex +1] === squares[392]) {
+          pacmanCurrentIndex = 364
+        }
+        break
+      case 40:
+        if (
+          pacmanCurrentIndex + width < width * width &&
+          !squares[pacmanCurrentIndex +width].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')
+        )
+        pacmanCurrentIndex += width
+        break
+    }
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+    pacDotEaten()
+    powerPelletEaten()
+    checkForGameOver()
+    checkForWin()
+  }
+  document.addEventListener('keyup', movePacman)
+
+  // gdy zje monetkę
+  function pacDotEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+      score++
+      scoreDisplay.innerHTML = score
+      squares[pacmanCurrentIndex].classList.remove('pac-dot')
+    }
+  }
+
+  //gdy zje monetkę mocy 
+  function powerPelletEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
+      score +=10
+      ghosts.forEach(ghost => ghost.isScared = true)
+      setTimeout(unScareGhosts, 10000)
+      squares[pacmanCurrentIndex].classList.remove('power-pellet')
+    }
+  }
+
+  //aby duchy przestały mrugać
+  function unScareGhosts() {
+    ghosts.forEach(ghost => ghost.isScared = false)
+  }
+
+  //tworzenie duchów przy pomocy konstruktorów
+  class Ghost {
+    constructor(className, startIndex, speed) {
+      this.className = className
+      this.startIndex = startIndex
+      this.speed = speed
+      this.currentIndex = startIndex
+      this.isScared = false
+      this.timerId = NaN
+    }
+  }
+
+  //wszystkie duchy
+  ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+    ]
+
+  //rysowanie duchów
+  ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.className)
+    squares[ghost.currentIndex].classList.add('ghost')
+    })
+
+  //losowe poruszanie duchów
+  ghosts.forEach(ghost => moveGhost(ghost))
+
+  function moveGhost(ghost) {
+    const directions =  [-1, +1, width, -width]
+    let direction = directions[Math.floor(Math.random() * directions.length)]
+
+    ghost.timerId = setInterval(function() {
+      //jeśli kolejne pole na które duch się poruszy nie mieści duchów innych albo ścian
+      if  (!squares[ghost.currentIndex + direction].classList.contains('ghost') &&
+        !squares[ghost.currentIndex + direction].classList.contains('wall') ) {
+          //usuń klasy duchów
+          squares[ghost.currentIndex].classList.remove(ghost.className)
+          squares[ghost.currentIndex].classList.remove('ghost', 'scared-ghost')
+          //porusz się na to pole
+          ghost.currentIndex += direction
+          squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
+      //w innym wypadku określ inny losowy kierunek ruchu
+      } else direction = directions[Math.floor(Math.random() * directions.length)]
+
+      //jeśli ducha da sie zjeść
+      if (ghost.isScared) {
+        squares[ghost.currentIndex].classList.add('scared-ghost')
+      }
+
+      //jeśli duch zostaje zjedzony
+      if(ghost.isScared && squares[ghost.currentIndex].classList.contains('pac-man')) {
+        squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
+        ghost.currentIndex = ghost.startIndex
+        score +=100
+        squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
+      }
+    checkForGameOver()
+    }, ghost.speed)
+  }
+
+  //sprawdź czy koniec gry
+  function checkForGameOver() {
+    if (squares[pacmanCurrentIndex].classList.contains('ghost') &&
+      !squares[pacmanCurrentIndex].classList.contains('scared-ghost')) {
+      ghosts.forEach(ghost => clearInterval(ghost.timerId))
+      document.removeEventListener('keyup', movePacman)
+      setTimeout(function(){ alert("Game Over"); }, 500)
+    }
+  }
+
+  //sprawdź czy wygrał gracz
+  function checkForWin() {
+    if (score === 274) {
+      ghosts.forEach(ghost => clearInterval(ghost.timerId))
+      document.removeEventListener('keyup', movePacman)
+      setTimeout(function(){ alert("You have WON!"); }, 500)
+    }
+  }
+})
