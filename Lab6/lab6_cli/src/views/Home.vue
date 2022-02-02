7bfb5ba30878c1c859ca254965cfa3c7ef7ee175
Robert Kolke <robertoskarkolke@gmail.com>
2022-02-02 23:25:08 +0100

    Lab 6 zrobione

---
+++ Lab6/lab6_cli/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/infa.jpg">
+    <HelloWorld msg="Lab 6 Vue"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+})
+</script>
